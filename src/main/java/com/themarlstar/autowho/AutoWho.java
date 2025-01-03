0d826151a168d717c7d331876aaadc039e0eadab
Marley Reeves <marley@marlstar.com>
2025-01-03 11:07:12 +1000

    Do some things other mods seem to all do

--- src/main/java/com/themarlstar/autowho/AutoWho.java
+++ src/main/java/com/themarlstar/autowho/AutoWho.java
@@ -1,7 +1,6 @@
 package com.themarlstar.autowho;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -10,11 +9,33 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 
+import java.io.File;
 
 
-@Mod(modid = "autowho", name = "AutoWho", version = "1.0")
+@Mod(
+        modid = AutoWho.modID,
+        name = AutoWho.modName,
+        version = AutoWho.version,
+        acceptedMinecraftVersions = "[1.8.9]",
+        clientSideOnly = true
+)
 public class AutoWho
 {
+    public static final String modID = "autowho";
+    public static final String modName = "AutoWho";
+    public static final String version = "1.0";
+    public static File jarFile;
+    public static AutoWho INSTANCE;
+
+    public AutoWho() {
+        INSTANCE = this;
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        this.jarFile = event.getSourceFile();
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
